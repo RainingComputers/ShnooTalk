44392c74a8ecd575d99a9e5c2ae122711103cd7f
RainingComputers <vishnu.vish.shankar@gmail.com>
2021-03-22 22:32:26 +0530

    Setup LLVM boilerplate

--- src/llvmgen.cpp
+++ src/llvmgen.cpp
@@ -1,5 +1,8 @@
 #include "llvmgen.hpp"
 
+using namespace llvm;
+using namespace llvm::sys;
+
 namespace llvmgen
 {
     icode::target_desc target_desc()
@@ -27,5 +30,72 @@ namespace llvmgen
         uhlltarget.default_int = icode::I32;
 
         return uhlltarget;
+    }
+
+    void gen_module(icode::module_desc module)
+    {
+        /* Setup LLVM context, module and builder */
+        std::unique_ptr<LLVMContext> llvm_context = std::make_unique<LLVMContext>();
+        std::unique_ptr<Module> llvm_module = std::make_unique<Module>("my cool jit", *llvm_context);
+        std::unique_ptr<IRBuilder<>> llvm_builder = std::make_unique<IRBuilder<>>(*llvm_context);
+
+        /* Loop through each function and convert mikuro IR to llvm IR */
+        for(auto func: module.functions)
+        {
+
+        }
+
+        /* Initialize the target registry etc */
+        InitializeAllTargetInfos();
+        InitializeAllTargets();
+        InitializeAllTargetMCs();
+        InitializeAllAsmParsers();
+        InitializeAllAsmPrinters();
+
+        /* Setup LLVM target triple */
+        auto target_triple = sys::getDefaultTargetTriple();
+        llvm_module->setTargetTriple(target_triple);
+
+        std::string error;
+        auto Target = TargetRegistry::lookupTarget(target_triple, error);
+
+        if (!Target) {
+            log::println(error);
+            throw log::internal_bug_error();
+        }
+
+        /* Setup LLVM data layout */
+        std::string CPU = "generic";
+        std::string features = "";
+    
+        TargetOptions opt;
+        auto RM = Optional<Reloc::Model>();
+        auto target_machine = Target->createTargetMachine(target_triple, CPU, features, opt, RM);
+    
+        llvm_module->setDataLayout(target_machine->createDataLayout());
+
+        /* Setup output object file and pass manager */
+        auto filename = module.name + ".o";
+        std::error_code EC;
+        raw_fd_ostream dest(filename, EC, sys::fs::OF_None);
+
+        if (EC)
+        {
+            // TODO: change this
+            log::println("Could not open file: " + EC.message());
+            throw log::internal_bug_error();
+        }
+
+        legacy::PassManager pass;
+        auto FileType = CGFT_ObjectFile;
+
+        if (target_machine->addPassesToEmitFile(pass, dest, nullptr, FileType))
+        {
+            log::println("LLVM target machine can't emit a file of this type");
+            throw log::internal_bug_error();
+        }
+
+        pass.run(*llvm_module);
+        dest.flush();
     }
 }
--- src/llvmgen.hpp
+++ src/llvmgen.hpp
@@ -1,11 +1,37 @@
 #ifndef LLVMGEN_HPP
 #define LLVMGEN_HPP
 
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+
 #include "icode.hpp"
+#include "log.hpp"
 
 namespace llvmgen
 {
     icode::target_desc target_desc();
+
+    void gen_module(icode::module_desc& module);
+
 }
 
 #endif
