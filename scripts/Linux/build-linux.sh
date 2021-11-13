76bdc990c4ca97a898f9558f842e344c83719c8f
RainingComputers <vishnu.vish.shankar@gmail.com>
2021-11-13 09:48:30 +0530

    Add MacOS release workflow

--- scripts/Linux/build-linux.sh
+++ scripts/Linux/build-linux.sh
@@ -1,6 +1,8 @@
 # This script should be run from the root of the project directory
 # After running this script, you will have a .tar.gz build in the bin/ directory
 
+set -xe
+
 echo '🤖 Downloading LLVM release'
 curl -OL https://github.com/llvm/llvm-project/releases/download/$1
 
@@ -11,6 +13,9 @@ tar -xf `basename $1` -C llvm --strip-components 1
 echo '🤖 Building compiler'
 make build LLVM_PATH=llvm
 BUILD_NAME=`cat build-name.txt`
+
+echo '🤖 Checking executable'
+bin/$BUILD_NAME/shtkc -version
 
 echo '🤖 Creating tar.gz'
 cd bin/
