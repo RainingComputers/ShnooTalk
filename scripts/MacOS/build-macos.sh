05bb5fa036fbbe1f3afd1f2c4d5ad5a377d50be1
shnoo <shnoo@shnoos-iMac.local>
2021-11-13 09:13:42 +0530

    Added executable check to macOS build script

--- scripts/MacOS/build-macos.sh
+++ scripts/MacOS/build-macos.sh
@@ -1,6 +1,8 @@
 # This script should be run from the root of the project directory
 # After running this script, you will have a .tar.gz build in the bin/ directory
 
+set -e
+
 echo '🤖 Installing LLVM using brew'
 brew install llvm@12
 
@@ -10,6 +12,9 @@ BUILD_NAME=`cat build-name.txt`
 
 echo '🤖 Rewiring and moving .dylib files'
 python3 scripts/MacOS/dylib_rewire.py bin/$BUILD_NAME/shtkc
+
+echo '🤖 Checking executable'
+bin/$BUILD_NAME/shtkc -version
 
 echo '🤖 Creating tar.gz'
 cd bin/
