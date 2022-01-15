ca835b967dfc40c27e136e7d901ce69c32e6a3e7
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-01-15 16:20:15 +0530

    Fixed code formatting

--- src/Translator/AllocPointer.cpp
+++ src/Translator/AllocPointer.cpp
@@ -12,7 +12,7 @@ void allocPointer(ModuleContext& ctx, const icode::Entry& e)
     Value* allocPointer = ctx.builder->CreateAlloca(dataTypeToLLVMType(ctx, e.op1.dtype));
 
     allocPointer = ctx.builder->CreatePtrToInt(allocPointer, dataTypeToLLVMType(ctx, icode::I64));
-    
+
     setLLVMValue(ctx, e.op1, allocPointer);
 }
 
@@ -21,11 +21,11 @@ void allocArrayPointer(ModuleContext& ctx, const icode::Entry& e)
     /* Converts ShnooTalk ALLOC_ARRAY_PTR to llvm ir */
 
     Type* bytesArrayType = ArrayType::get(Type::getInt8Ty(*ctx.context), e.op2.val.bytes);
-    
+
     Value* allocPointer = ctx.builder->CreateAlloca(bytesArrayType);
     allocPointer = ctx.builder->CreateBitCast(allocPointer, dataTypeToLLVMPointerType(ctx, e.op1.dtype));
 
     allocPointer = ctx.builder->CreatePtrToInt(allocPointer, dataTypeToLLVMType(ctx, icode::I64));
-    
+
     setLLVMValue(ctx, e.op1, allocPointer);
 }
