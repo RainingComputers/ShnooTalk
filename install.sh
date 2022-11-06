1f79c44835e4c2af14dbb587f937933f20cab756
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-11-06 22:36:19 +0530

    ci: Update install script

--- install.sh
+++ install.sh
@@ -14,18 +14,29 @@ REPOSITORY="https://github.com/RainingComputers/Shnootalk"
 TAGS=`git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' $REPOSITORY`
 VERSION=`echo ${TAGS##*/}`
 BIN_DIR_NAME=`echo "shtkc-$VERSION-$PLATFORM"`
-ASSET_NAME=`echo "$BIN_DIR_NAME.tar.gz"`
+COMPILER_TAR_NAME=`echo "$BIN_DIR_NAME.tar.gz"`
+STDLIB_TAR_NANE=`echo shnootalk-stdlib-$VERSION.tar.gz`
 
-echo "🤖 Downloading $ASSET_NAME"
-RELEASE_LINK="github.com/rainingcomputers/shnootalk/releases/latest/download/$ASSET_NAME"
-curl -OL $RELEASE_LINK
+echo "🤖 Downloading $COMPILER_TAR_NAME"
+COMPILER_RELEASE_LINK="github.com/rainingcomputers/shnootalk/releases/latest/download/$COMPILER_TAR_NAME"
+curl -OL $COMPILER_RELEASE_LINK
 
-echo "🤖 Extracting $ASSET_NAME"
-tar -xf $ASSET_NAME
+echo "🤖 Extracting $COMPILER_TAR_NAME"
+tar -xf $COMPILER_TAR_NAME
 
 echo "🤖 Installing compiler"
 mv $BIN_DIR_NAME/shtkc /usr/local/bin
 
+echo "🤖 Downloading $STDLIB_TAR_NAME"
+STDLIB_RELEASE_LINK="github.com/rainingcomputers/shnootalk/releases/latest/download/$STDLIB_TAR_NAME"
+curl -OL $STDLIB_RELEASE_LINK
+
+echo "🤖 Extracting $STDLIB_TAR_NAME"
+tar -xf $STDLIB_TAR_NAME
+
+echo "🤖 Installing compiler"
+mv stdlib/ /usr/local/lib/shnootalk-$VERSION/
+
 echo "🤖 Removing temp files"
 rm -rf $BIN_DIR_NAME
-rm -f $ASSET_NAME
+rm -f $COMPILER_TAR_NAME
