1e849751fc1560d81ecd61bb60cef6afc5d2d292
RainingComputers <vishnu.vish.shankar@gmail.com>
2023-02-17 18:44:36 +0530

    compiler: Fix bug in parser

--- compiler/src/Parser/Literal.cpp
+++ compiler/src/Parser/Literal.cpp
@@ -8,7 +8,7 @@ void literal(parser::ParserContext& ctx)
                                     token::HEX_LITERAL,
                                     token::BIN_LITERAL };
 
-    if (ctx.accept(token::MINUS) | ctx.accept(token::PLUS))
+    if (ctx.accept(token::MINUS) || ctx.accept(token::PLUS))
     {
         ctx.addNode(node::UNARY_OPR);
         ctx.expect(literals, 5);
